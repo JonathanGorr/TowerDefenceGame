ed2311da91523bc0b61f464348243adf0fc1d48d
Jonathan Gorr <jonathangorr@gmail.com>
2015-04-05 19:34:58 -0400

    Some environment changes

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	[HideInInspector]
+	public int currency = 10;
+
+	public void NextLevel()
+	{
+	}
+
+	public void Pause()
+	{
+	}
+
+	public void Resume()
+	{
+	}
+
+	public void Quit()
+	{
+	}
+
+	public void ReturnToMenu()
+	{
+	}
+}
